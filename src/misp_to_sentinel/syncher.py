297eee9f6c4eb9466b8070b94f58566276f22112
Nicolas Velasquez <gnicolax@gmail.com>
2023-04-05 16:38:42 +0200

    Better var names

--- src/misp_to_sentinel/syncher.py
+++ src/misp_to_sentinel/syncher.py
@@ -8,13 +8,14 @@ from misp_to_sentinel.utils.environ_utils import load_env_variable
 async def sync():
     """Sync MISP to Sentinel."""
     # Retrieve from MISP
-    misp = MISPConnector(
+    misp_connector = MISPConnector(
         misp_base_url=load_env_variable("MISP_BASE_URL"),
         misp_ca_bundle_path=load_env_variable("MISP_CA_BUNDLE_PATH"),
         misp_key=load_env_variable("MISP_KEY"),
         misp_label=load_env_variable("MISP_LABEL"),
     )
-    misp_attributes = await misp.get_attributes_with_stix2_patterns(
+
+    misp_attributes = await misp_connector.get_attributes_with_stix2_patterns(
         look_back_days=load_env_variable("MISP_LOOK_BACK_DAYS"),
     )
 

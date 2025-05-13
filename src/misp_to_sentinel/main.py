69c3b43a55687dbac5edbd9aa18de0ae42e0412e
Nicolas Velasquez <gnicolax@gmail.com>
2025-05-13 15:33:37 +0200

    fix(main): move logging setting to main

--- src/misp_to_sentinel/main.py
+++ src/misp_to_sentinel/main.py
@@ -6,22 +6,34 @@ In-house script for pushing ICC MISP IOCs onto MS Sentinel. MS' tool is overly c
 
 import asyncio
 import logging
+from datetime import date
+
+from dotenv import load_dotenv
+from filelock import FileLock, Timeout
 
 from misp_to_sentinel.syncher import sync
+
+logger = logging.getLogger(__name__)
 
 
 def main():
     """Main script/function of the whole project."""
+    load_dotenv()
     logging.basicConfig(
+        filename=f"logs/sync_ms_announcements_{date.today()}.log",  # noqa: DTZ011
         format="%(asctime)s %(levelname)-10s [%(filename)s:%(lineno)d %(funcName)s] %(message)s",
         level=logging.INFO,
     )
 
-    logging.info("Starting")
-
-    asyncio.run(sync())
-
-    logging.info("Finished")
+    try:
+        logger.info("Acquiring lock")
+        with FileLock("sync_ms_announcements.lock", timeout=1):
+            logger.info("Lock acquired")
+            logger.info("Starting")
+            asyncio.run(sync())
+            logger.info("Finished")
+    except Timeout:
+        logger.warning("Couldn't acquire lock, another instance is running. Exiting.")
 
 
 if __name__ == "__main__":

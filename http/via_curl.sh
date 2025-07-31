acfbfa7becc27e186e3b5c547faab7e764c77c6a
Nicolas Velasquez <gnicolax@gmail.com>
2025-07-31 11:22:52 +0200

    feat(http): add additional curl requests for event search with JSON format

--- http/via_curl.sh
+++ http/via_curl.sh
@@ -30,8 +30,22 @@ curl -sS -O -w "\nResponse code: %{http_code}, Time: %{time_total}s\n" \
   -d '{"timestamp": "6d", "published": true, "returnFormat": "stix2"}'
 
 curl -sS -O -w "\nResponse code: %{http_code}, Time: %{time_total}s\n" \
-  -X POST "https://commonsecure.unicc.org/events/restSearch" \
+  -X POST "https://commonsecure.unicc.org/attributes/restSearch" \
   -H 'authorization: '${MISP_KEY} \
   -H "Accept: application/json" \
   -H "Content-Type: application/json" \
-  -d '{"timestamp": "15d", "published": true, "returnFormat": "stix2"}'
+  -d '{"timestamp": "1d", "published": true, "returnFormat": "json"}'
+
+curl -sS -O -w "\nResponse code: %{http_code}, Time: %{time_total}s\n" \
+  -X POST "https://commonsecure.unicc.org/attributes/restSearch" \
+  -H 'authorization: '${MISP_KEY} \
+  -H "Accept: application/json" \
+  -H "Content-Type: application/json" \
+  -d '{"timestamp": "5d", "published": true, "returnFormat": "json"}'
+
+curl -sS -O -w "\nResponse code: %{http_code}, Time: %{time_total}s\n" \
+  -X POST "https://commonsecure.unicc.org/attributes/restSearch" \
+  -H 'authorization: '${MISP_KEY} \
+  -H "Accept: application/json" \
+  -H "Content-Type: application/json" \
+  -d '{"timestamp": "6d", "published": true, "returnFormat": "json"}'
